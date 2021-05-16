a2a500574599c6951b64e04abf60086f81ec5ad7
Roman <46456972+RomanKasko@users.noreply.github.com>
2021-05-16 21:55:50 +0300

    Articles belongs to user

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,10 @@ class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :img_elements
+  belongs_to :user
+  validates_presence_of :user_id
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
 end
