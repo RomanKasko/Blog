c4681ca8b3ec7e0d3b33cac80dbdb64bddf4a8a4
Roman <46456972+RomanKasko@users.noreply.github.com>
2021-05-18 13:46:15 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home_page#index"
+  root to: "home#index"
   resources :img_elements
-
+  get 'find', to: 'articles#find'
   resources :articles do
     resources :comments
   end
+  resources :tags, only: [:show]
 end
+
